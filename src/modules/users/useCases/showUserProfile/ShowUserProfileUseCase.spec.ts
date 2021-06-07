ca3969382be243dd5f31ab4f9c7aa294898382f7
kaynansc <kaynansc@gmail.com>
2021-06-07 20:12:23 -0300

    feat: add unit test - show user profile

---
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.spec.ts
@@ -0,0 +1,40 @@
+import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository";
+import { CreateUserUseCase } from "../createUser/CreateUserUseCase";
+import { ShowUserProfileError } from "./ShowUserProfileError";
+import { ShowUserProfileUseCase } from "./ShowUserProfileUseCase"
+
+let inMemoryUsersRepository: InMemoryUsersRepository;
+let showUserProfileUseCase: ShowUserProfileUseCase;
+let createUserUseCase: CreateUserUseCase;
+
+describe("Show User Profile", () => {
+  beforeEach(() => {
+    inMemoryUsersRepository = new InMemoryUsersRepository();
+    showUserProfileUseCase = new ShowUserProfileUseCase(inMemoryUsersRepository);
+    createUserUseCase = new CreateUserUseCase(inMemoryUsersRepository);
+  })
+
+  it("should be able to show user profile with valid id", async () => {
+    const { id } = await createUserUseCase.execute({
+      name: "User test",
+      email: "test@test.com.br",
+      password: "passwordsecret",
+    });
+
+    const user = await showUserProfileUseCase.execute(id || "");
+
+    expect(user.id).toEqual(id);
+  })
+
+  it("should not be able to show user profile with invalid id", () => {
+    expect(async () => {
+      const { id } = await createUserUseCase.execute({
+        name: "User test",
+        email: "test@test.com.br",
+        password: "passwordsecret",
+      });
+
+      await showUserProfileUseCase.execute("invalidID");
+    }).rejects.toBeInstanceOf(ShowUserProfileError);
+  })
+})
